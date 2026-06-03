e01ba63d39ae142add45e5f1422112acb4558201
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-06-03 23:43:41 +0530

    Implemented Proper Logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#[macro_use]
+extern crate log;
+extern crate simplelog;
 
+use simplelog::*;
 fn main() {
+    // intiate logging
+    simplelog::CombinedLogger::init(vec![
+        simplelog::WriteLogger::new(
+            simplelog::LevelFilter::Debug,
+            simplelog::Config::default(),
+            std::fs::File::create("env_explorer.log").unwrap(),
+        ),
+        simplelog::SimpleLogger::new(simplelog::LevelFilter::Debug, simplelog::Config::default()),
+    ])
+    .unwrap();
+    info!("Application started");
+
     env_explorer_lib::run()
 }
