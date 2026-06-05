146a22e521e4db3e0cfdade93c736a1a5c098518
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-06-05 14:42:54 +0530

    Added a Loader

--- src/routes/home/windows/projectWindow.svelte
+++ src/routes/home/windows/projectWindow.svelte
@@ -4,8 +4,12 @@
     let state = "loading" ;
     
 </script>
-<div class="window-root">
+<div class="window-root flex justify-center items-center">
     {#if state = "loading"}
-        <Spinner />
+    <!-- Keep this div full seze of avilable space and make it center i need spinner in center -->
+    <div class="flex flex-col justify-center items-center w-full h-[calc(100vh-88px)]">
+        <Spinner type="bars" color="#72ddc3" size="16"/>
+        <span class="text-white mt-2">Scanning...</span>
+    </div>
     {/if}
 </div>
