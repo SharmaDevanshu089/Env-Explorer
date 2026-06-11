70d36355c0daf23add04584a78f9fda4997f777c
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-06-11 16:14:21 +0530

    built a function of total variables

--- src/routes/home/windows/analyticWindow.svelte
+++ src/routes/home/windows/analyticWindow.svelte
@@ -4,6 +4,7 @@
     // Funtion to get total number of Variables
     let variable_count = 0;
     let env_files = [];
+    let count = 0;
     async function get_env_list() {
         try{
             let reading_from_os = await invoke("read_env_config");
@@ -15,8 +16,11 @@
         }
         for (const file of env_files) {
             console.log(file.name);
-            let
+            let no_of_env_variables = await invoke("count_env_vars", { path: file.path });
+            console.log(no_of_env_variables);
+            count = count + no_of_env_variables;
         }
+        console.log(count);
     }
 
 
